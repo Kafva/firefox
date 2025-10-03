1aee57a2b5f30d43f4051742424ad88d9f8a93b6
Kafva <kafva.one@gmail.com>
2025-10-03 07:14:23 +0200

    Use 1.89 rust
    FIREFOX_NIGHTLY_143_END does not compile with 1.90, stay on 1.89
    |  help: the same lifetime is referred to in inconsistent ways,
    |  making the signature confusing, help: use `'_` for type paths

--- scripts/rustup.sh
+++ scripts/rustup.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+# FIREFOX_NIGHTLY_143_END does not compile with 1.90, stay on 1.89
+# |  help: the same lifetime is referred to in inconsistent ways,
+# |  making the signature confusing, help: use `'_` for type paths
+VERSION=1.89
+
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | \
 bash -s -- -y \
            -t wasm32-unknown-unknown \
-           --default-toolchain stable
+           --default-toolchain $VERSION
 
 if [ $(uname) = Darwin ]; then
     "$HOME/.cargo/bin/rustup" target add aarch64-apple-darwin
